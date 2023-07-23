4f5c7224136a4d776500240445ffe0e5e00c2d55
Sam Harrison <sammatthewharrison@gmail.com>
2023-07-23 13:56:26 +0100

    fix: update indicators on mobile

--- src/components/ImageCarousel.vue
+++ src/components/ImageCarousel.vue
@@ -1,9 +1,13 @@
 <script setup>
 import { ref, nextTick, onMounted, watch } from 'vue'
+import { useElementApi } from '@baleada/vue-features';
 
 const props = defineProps({
     images: Array
 })
+
+const items = useElementApi({ kind: 'list' })
+const container = useElementApi()
 
 const carousel = ref(null)
 const mainImage = ref(null)
@@ -11,7 +15,6 @@ const mainImageIndex = ref(0)
 const width = ref(0)
 const scrollable = ref(false)
 const touchable = ref(false)
-const loading = ref(true)
 
 function setScrollTouch() {
     width.value = mainImage.value.clientWidth;
@@ -25,20 +28,15 @@ function setMainIndex(index) {
         top: 0,
         left: width.value * index
     });
-    mainImageIndex.value = index
 }
 
 function changeImage(index) {
     let scrollCoordinate = carousel.value.scrollLeft + width.value * index;
 
     if (mainImageIndex.value + index < 0) {
-        mainImageIndex.value = props.images.length - 1;
         scrollCoordinate = carousel.value.scrollLeft + width.value * props.images.length;
     } else if (mainImageIndex.value + index > props.images.length - 1) {
-        mainImageIndex.value = 0;
         scrollCoordinate = carousel.value.scrollRight + width.value * props.images.length;
-    } else {
-        mainImageIndex.value += index
     }
 
     carousel.value.focus();
@@ -67,25 +65,37 @@ onMounted(() => {
     nextTick(() => {
         window.addEventListener('resize', setScrollTouch)
     })
-    loading.value = false
+
+    let observer = new IntersectionObserver(entries => {
+        const { target } = entries[0]
+        const index = items.elements.value.findIndex((el) => el.isSameNode(target))
+        mainImageIndex.value = index
+
+    }, {
+        threshold: 1,
+        root: container.element.value
+    })
+
+    for (const element of items.elements.value) {
+        observer.observe(element)
+    }
+
 })
 </script>
 
 <template>
     <div>
         <div ref="mainImage" class="relative">
-            <div v-if="loading">
-            </div>
-            <div v-else>
+            <div>
                 <button v-if="scrollable && !touchable" type="button" aria-label="Toggle previous image"
                     class="absolute left-2 top-1/2 flex -translate-y-1/2 border bg-white transition"
                     @click.prevent="changeImage(-1)">
                     <img src="/icons/chevron-left.svg" class="inline h-8 w-8" :aria-hidden="true" />
                 </button>
                 <div class="carousel no-scrollbar grid min-w-full snap-x snap-mandatory auto-cols-max grid-flow-col overflow-x-auto overflow-y-hidden"
-                    ref="carousel" tabindex="0">
-                    <div v-for="(image, index) in images" :key="index" :style="`width: ${width}px; height: ${width}px`"
-                        class="snap-center">
+                    ref="carousel" :ref="container.ref" tabindex="0">
+                    <div v-for="(image, index) in images" :ref="items.getRef(index)" :key="index"
+                        :style="`width: ${width}px; height: ${width}px`" class="snap-center">
                         <img :src="image" class="object-cover w-full h-full rounded-lg" />
                     </div>
                 </div>
